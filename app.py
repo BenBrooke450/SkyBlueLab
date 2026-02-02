98b3d8458eea6b48d4d538d2aaef00b51fc7a5ce
BenBrooke450 <bensir@mail.com>
2026-02-02 18:11:56 +0100

    UPDATE

--- app.py
+++ app.py
@@ -3,8 +3,12 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="SkyBlueLab", page_icon="🔬")
+st.set_page_config(page_title="SkyBlueLab")
 st.title("SkyBlueLab: Vision AI")
+st.sidebar.image("Website/Cropped.png", width=50)
+
+
+
 
 # Sidebar for settings
 with st.sidebar:
