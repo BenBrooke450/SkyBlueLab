94be5fef93cf6f59c1bbcd1979fc3f00037ff2fc
BenBrooke450 <bensir@mail.com>
2026-03-25 22:09:27 +0100

    UPDATE

--- BlueOcean/ModernBERT-base.py
+++
@@ -1,7 +0,0 @@
-
-from transformers import AutoTokenizer, AutoModelForMaskedLM
-
-model_id = "answerdotai/ModernBERT-base"
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForMaskedLM.from_pretrained(model_id)
